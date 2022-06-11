89631bd082bbcb063500ee82871e52d7d561abb5
unknown <subramanya25@outlook.com>
2022-06-11 13:43:13 +0530

    initila

---
+++ dbo/Tables/tbl_registartion.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tbl_registartion] (
+    [id]            INT          IDENTITY (1, 1) NOT NULL,
+    [Firstname]     VARCHAR (50) NULL,
+    [Lastname]      VARCHAR (50) NULL,
+    [Emailid]       VARCHAR (50) NULL,
+    [Mobileno]      VARCHAR (12) NULL,
+    [Date_of_Birth] DATETIME     NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
